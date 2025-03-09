d81c9f5294f74b778afcc461e9545ad98377fdf2
WayneTan <Alger7865@gmail.com>
2025-03-10 01:17:08 +0800

    1. Extract the serializer interface, Define the 2 serialize way: Json, JDK. 2. implement 2 Serialize way. And need to specify the response result in RPCResponse, because JSON couldn't recognize the Json. 3. We need to define the Message Framework in Decoder class and Encoder class. so that message could be transmitted in a low cost. 4. Netty or fastJson dependency should be added to Pom for every module.( be careful !!!) 5. Please do more analysis in the Json recognize object part.

--- RPC-Server/src/main/java/RPCResponse/NettyRPCServerInitializer.java
+++ RPC-Server/src/main/java/RPCResponse/NettyRPCServerInitializer.java
@@ -1,5 +1,8 @@
 package RPCResponse;
 
+import SerializerUtil.JsonSerializer;
+import SerializerUtil.RPCDecoder;
+import SerializerUtil.RPCEncoder;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
@@ -20,17 +23,18 @@ public class NettyRPCServerInitializer extends ChannelInitializer<SocketChannel>
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         ChannelPipeline pipeline = socketChannel.pipeline();
 
-        pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
-        pipeline.addLast(new LengthFieldPrepender(4));
+//        pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+//        pipeline.addLast(new LengthFieldPrepender(4));
+//        pipeline.addLast(new ObjectEncoder());
+//        pipeline.addLast(new ObjectDecoder(new ClassResolver() {
+//            @Override
+//            public Class<?> resolve(String className) throws ClassNotFoundException {
+//                return Class.forName(className);
+//            }
+//        }));
 
-        pipeline.addLast(new ObjectEncoder());
-        pipeline.addLast(new ObjectDecoder(new ClassResolver() {
-            @Override
-            public Class<?> resolve(String className) throws ClassNotFoundException {
-                return Class.forName(className);
-            }
-        }));
-
+        pipeline.addLast(new RPCDecoder());
+        pipeline.addLast(new RPCEncoder(new JsonSerializer()));
         pipeline.addLast(new NettyRPCServerHandler(serviceMap));
     }
 }
